76fe3ded8f2ff49fe71669ad038d37b7eb32feb2
leleliu008 <792793182@qq.com>
2020-07-01 17:16:23 +0800

    optimize

--- Formula/adoptopenjdk8.rb
+++ Formula/adoptopenjdk8.rb
@@ -1,20 +1,20 @@
-class AndroidSdk < Formula
-  desc     "Android Software Develpment Kit"
-  homepage "https://developer.android.google.cn/studio#cmdline-tools"
-  version  "6609375"
-  
+class Adoptopenjdk8 < Formula
+  desc     "Adopt OpenJDK version 8" 
+  homepage "https://adoptopenjdk.net"
+  version  "jdk8u252-b09_openj9-0.20.0"
+ 
+  #https://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64 
   on_linux do
-    url "https://dl.google.com/android/repository/commandlinetools-linux-#{version}_latest.zip"
-    sha256 "89f308315e041c93a37a79e0627c47f21d5c5edbe5e80ea8dc0aac8a649e0e92"
+    url "http://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64/linux/OpenJDK8U-jdk_x64_linux_openj9_8u252b09_openj9-0.20.0.tar.gz"
+    sha256 "910ae847109a6dd1b6cf69baa7615ea2cce8cff787e5a9349a5331ce7604f3a5"
   end
   
   on_macos do
-    url "https://dl.google.com/android/repository/commandlinetools-mac-#{version}_latest.zip"
-    sha256 "2c3822db1c916655223e5ee8ce0fbf6b73d0b99012045c9dc8eaa6a5736c0c55"
+    url "http://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64/mac/OpenJDK8U-jdk_x64_mac_openj9_8u252b09_openj9-0.20.0.tar.gz"
+    sha256 "f522061a23290bce3423e49025a95b6e78d6f30e2741817e83c8fdba4c0c4ae7"
   end
   
   def install
-    #https://stackoverflow.com/questions/60440509/android-command-line-tools-sdkmanager-always-shows-warning-could-not-create-se
     system "mkdir -p #{prefix}/cmdline-tools/tools"
     system "cp -r *  #{prefix}/cmdline-tools/tools"
     system "printf y | #{prefix}/cmdline-tools/tools/bin/sdkmanager \"platforms;android-28\""
