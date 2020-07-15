5b6c8ad83e6d9d8de506b89eb2d65b946e2d28b4
leleliu008 <792793182@qq.com>
2020-07-15 13:24:52 +0800

    optimize

--- Formula/walle-cli.rb
+++ Formula/walle-cli.rb
@@ -2,7 +2,7 @@ class WalleCli < Formula
   desc     "Android Signature V2 Scheme签名下的新一代渠道包打包神器"
   homepage "https://github.com/Meituan-Dianping/walle"
   url      "https://github.com/leleliu008/walle-cli-wrapper/releases/download/v1.1.6/walle-cli-1.1.6.tar.gz"
-  sha256   "eef6d72c11e0ab35ef6937eea76087be1ae71a415ab12f85968ad6d627033284"
+  sha256   "511d8977e294ef5795e079e8fa0137a11592e6d2f2148523267cd4fcd6233184"
 
   def install
     bin.install "bin/walle"
