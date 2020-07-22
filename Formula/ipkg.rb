d780d916bbe9c8294662eb76042d54450c0ce00f
leleliu008 <792793182@qq.com>
2020-07-23 01:32:56 +0800

    add ipkg

---
+++ Formula/ipkg.rb
@@ -0,0 +1,19 @@
+class Ipkg < Formula
+  desc     "Package manager for Xcode to build C/C++ project"
+  homepage "https://github.com/leleliu008/ipkg"
+  url      "https://github.com/leleliu008/ipkg/releases/download/v0.1.0/ipkg-0.1.0.tar.gz"
+  sha256   "5032219ae401dbaf7fd9edc0cbe0ad794bf07c45521a579e283f02068f184a17"
+  head     "https://github.com/leleliu008/ipkg.git"
+  
+  depends_on "curl"
+  depends_on "bat"
+  
+  def install
+    bin.install "bin/ipkg"
+    zsh_completion.install "zsh-completion/_ipkg" => "_ipkg"
+  end
+
+  test do
+    system "#{bin}/ipkg", "--help"
+  end
+end
