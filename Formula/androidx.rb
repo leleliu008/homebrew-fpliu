b2be1c2a18679e3e953755aa786c1dc11bfb28d4
leleliu008 <792793182@qq.com>
2020-07-25 14:20:55 +0800

    optimize

--- Formula/androidx.rb
+++ Formula/androidx.rb
@@ -2,7 +2,7 @@ class Androidx < Formula
   desc     "Android project helper"
   homepage "https://github.com/leleliu008/androidx"
   url      "https://github.com/leleliu008/androidx/releases/download/v0.1.0/androidx-0.1.0.tar.gz"
-  sha256   "2544aaddb6527392a5170b06a0dda94b97c1e869b8f1fb0fb8918760a449baea"
+  sha256   "16bfb70ac96b96421bca9d47df6b473532caf2ac8ce8753022f66bbccc31b8d1"
   head     "https://github.com/leleliu008/androidx.git"
   
   depends_on "android-sdk"
