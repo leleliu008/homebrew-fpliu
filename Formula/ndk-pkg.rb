c5245aba27bd6f9f819b653ba269867c7f4c4fb3
leleliu008 <792793182@qq.com>
2020-11-03 12:48:38 +0800

    optimize

--- Formula/ndk-pkg.rb
+++ Formula/ndk-pkg.rb
@@ -2,7 +2,7 @@ class NdkPkg < Formula
   desc     "Package manager for Android NDK"
   homepage "https://github.com/leleliu008/ndk-pkg"
   url      "https://github.com/leleliu008/ndk-pkg/releases/download/v0.1.0/ndk-pkg-0.1.0.tar.gz"
-  sha256   "d642821e6dd50732f0ded9fca45637d428af999c29d2023bacf5389f985ed10a"
+  sha256   "bf11ca8cf08305e1188e5c020d59a94a447067a45e0ce768070cbc0b9dffa73d"
   head     "https://github.com/leleliu008/ndk-pkg.git"
   depends_on "curl"
 
