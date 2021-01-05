21565d94e6129c45363f1060b8e44646314ce1f3
leleliu008 <792793182@qq.com>
2021-01-05 19:19:25 +0800

    optimize

--- Formula/ipkg.rb
+++ Formula/ipkg.rb
@@ -2,7 +2,7 @@ class Ipkg < Formula
   desc     "Package manager for Xcode to build C/C++ project"
   homepage "https://github.com/leleliu008/ipkg"
   url      "https://github.com/leleliu008/ipkg/releases/download/v0.1.0/ipkg-0.1.0.tar.gz"
-  sha256   "c482cc3d1fa9f5cc771ed6f20e57a88939e826f0a5a86e7380fa0e4062e01f19"
+  sha256   "61ccb4a434ddf672be6bc7218c4c54471b26bc46915388c13a1c409c67b15a73"
   head     "https://github.com/leleliu008/ipkg.git"
   
   depends_on "curl"
