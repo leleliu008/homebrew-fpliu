bee5d4d4c84bb726acdcf628918393ecf3d9d99c
leleliu008 <792793182@qq.com>
2021-01-17 13:34:31 +0800

    optimize

--- Formula/adoptopenjdk8.rb
+++ Formula/adoptopenjdk8.rb
@@ -1,17 +1,17 @@
 class Adoptopenjdk8 < Formula
   desc     "Adopt OpenJDK version 8" 
   homepage "https://adoptopenjdk.net"
-  version  "jdk8u252-b09_openj9-0.20.0"
+  version  "8u275b01_openj9-0.23.0"
  
   #https://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64 
   on_linux do
-    url "http://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64/linux/OpenJDK8U-jdk_x64_linux_openj9_8u252b09_openj9-0.20.0.tar.gz"
-    sha256 "910ae847109a6dd1b6cf69baa7615ea2cce8cff787e5a9349a5331ce7604f3a5"
+    url "http://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64/linux/OpenJDK8U-jdk_x64_linux_openj9_8u275b01_openj9-0.23.0.tar.gz"
+    sha256 "8bb74ff7ab2f64ced23212ee0520295c01b6f2d7558fd6e488a8ca795ced1ebd"
   end
   
   on_macos do
-    url "http://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64/mac/OpenJDK8U-jdk_x64_mac_openj9_8u252b09_openj9-0.20.0.tar.gz"
-    sha256 "f522061a23290bce3423e49025a95b6e78d6f30e2741817e83c8fdba4c0c4ae7"
+    url "http://mirrors.cnnic.cn/AdoptOpenJDK/8/jdk/x64/mac/OpenJDK8U-jdk_x64_mac_openj9_8u275b01_openj9-0.23.0.tar.gz"
+    sha256 "0e19282fe1dae272f1383f726cc6fc70d77816bebe07e0959ac2c9b9b711f709"
   end
   
   def install
