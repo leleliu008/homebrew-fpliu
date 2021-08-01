fad2d50b87315448195ed1fdca23280c09459ab2
leleliu008 <leleliu008@gmail.com>
2021-08-01 18:54:45 +0800

    publish new version zpkg-0.1.0

--- Formula/zpkg.rb
+++ Formula/zpkg.rb
@@ -2,7 +2,7 @@ class Zpkg < Formula
   desc     "Lightweight, portable package manager for UNIX"
   homepage "https://github.com/leleliu008/zpkg"
   url      "https://github.com/leleliu008/zpkg/releases/download/v0.1.0/zpkg-0.1.0.tar.gz"
-  sha256   "4ff0682f8d7863703d09931c95d76c029bfb4d5770ffa24286ab9db2a41abcce"
+  sha256   "e4f49362fca1ff489b3e003e7a82c80a76702576a91482a322096f5258d8cef8"
   head     "https://github.com/leleliu008/zpkg.git"
 
   def install
