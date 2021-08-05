3e1e4e73ef1faba18dd92a3c68224c70f38163ad
leleliu008 <leleliu008@gmail.com>
2021-08-05 20:50:08 +0800

    publish new version ipkg-0.1.0

--- Formula/ipkg.rb
+++ Formula/ipkg.rb
@@ -2,7 +2,7 @@ class Ipkg < Formula
   desc     "Package manager for Xcode to build C/C++ project"
   homepage "https://github.com/leleliu008/ipkg"
   url      "https://github.com/leleliu008/ipkg/releases/download/v0.1.0/ipkg-0.1.0.tar.gz"
-  sha256   "bb696be3160d11b099ab9869b2ed326df3653d06cd1f255bebe9a6c27bfa922d"
+  sha256   "24050c2f033433f52abb1a4c4142b7742cea8ac0afd820af01ef426e1ab4bf5e"
   head     "https://github.com/leleliu008/ipkg.git"
   
   def install
