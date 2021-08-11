879894c41c81078ed69f1e9c0f7f74a24e2d04fe
leleliu008 <leleliu008@gmail.com>
2021-08-11 18:26:55 +0800

    optimize

--- Formula/androidx.rb
+++ Formula/androidx.rb
@@ -5,12 +5,6 @@ class Androidx < Formula
   sha256   "321b506b4492ae474b6a9af9fd717b5a39daf28d48f4c5392c543ac406aea2b8"
   head     "https://github.com/leleliu008/androidx.git"
   
-  depends_on "android-sdk"
-  depends_on "qihu360jiagubao"
-  depends_on "andresguard-cli"
-  depends_on "walle-cli"
-  depends_on "p7zip"
-  
   def install
     bin.install "bin/androidx"
     zsh_completion.install "zsh-completion/_androidx" => "_androidx"
