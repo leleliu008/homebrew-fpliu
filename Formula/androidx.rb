f593ac636cff8a4b2c5f3f5f4dad870ddba069fb
leleliu008 <leleliu008@gmail.com>
2021-08-11 18:22:01 +0800

    publish new version androidx-0.1.0

--- Formula/androidx.rb
+++ Formula/androidx.rb
@@ -2,7 +2,7 @@ class Androidx < Formula
   desc     "Android project helper"
   homepage "https://github.com/leleliu008/androidx"
   url      "https://github.com/leleliu008/androidx/releases/download/v0.1.0/androidx-0.1.0.tar.gz"
-  sha256   "16bfb70ac96b96421bca9d47df6b473532caf2ac8ce8753022f66bbccc31b8d1"
+  sha256   "321b506b4492ae474b6a9af9fd717b5a39daf28d48f4c5392c543ac406aea2b8"
   head     "https://github.com/leleliu008/androidx.git"
   
   depends_on "android-sdk"
