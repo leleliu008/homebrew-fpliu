ff15c2c448a6554e952aa58da9ea996e6611d1cd
leleliu008 <leleliu008@gmail.com>
2021-08-14 02:16:48 +0800

    publish new version AndResGuard-cli-1.2.16

--- Formula/andresguard-cli.rb
+++ Formula/andresguard-cli.rb
@@ -2,7 +2,7 @@ class AndresguardCli < Formula
   desc     "Proguard resource for Android by wechat team"
   homepage "https://github.com/shwenzhang/AndResGuard"
   url      "https://github.com/leleliu008/AndResGuard-cli-wrapper/releases/download/v1.2.16/AndResGuard-cli-1.2.16.tar.gz"
-  sha256   "434acce2989105264660b241884afbbeb6f93d0b63319e0c69f3275e4e4a85be"
+  sha256   "0028aafc12cf099f5a3c3002bde05832b470d6a2c7005d54ca0cb7c6c5ce8e9a"
 
   depends_on "p7zip"
 
