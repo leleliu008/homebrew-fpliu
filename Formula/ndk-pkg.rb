31a5702a9dde9f7e3654536f4ead5b5571860d5a
leleliu008 <leleliu008@gmail.com>
2022-04-14 12:32:04 +0800

    publish new version 0.1.33

--- Formula/ndk-pkg.rb
+++ Formula/ndk-pkg.rb
@@ -1,8 +1,8 @@
 class NdkPkg < Formula
   desc     "Package manager for Android NDK"
   homepage "https://github.com/leleliu008/ndk-pkg"
-  url      "https://github.com/leleliu008/ndk-pkg/releases/download/v0.1.32/ndk-pkg-0.1.32.tar.gz"
-  sha256   "f838dc2a770ce4c05e247583d1950402cab3efff9634888f7eb9d0959490b5a1"
+  url      "https://github.com/leleliu008/ndk-pkg/releases/download/v0.1.33/ndk-pkg-0.1.33.tar.gz"
+  sha256   "035ea84a34c25b3ad6bb3f488f2a1de7f2bf6c41f69b21eeb2e5bd1695eea076"
   head     "https://github.com/leleliu008/ndk-pkg.git"
 
   def install
