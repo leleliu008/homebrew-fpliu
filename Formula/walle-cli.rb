374ecbeca24912fcd9d28d3f1cae279e1300ef7b
leleliu008 <leleliu008@gmail.com>
2022-05-04 21:11:28 +0800

    publish new version walle-cli 1.1.6

--- Formula/walle-cli.rb
+++ Formula/walle-cli.rb
@@ -2,7 +2,7 @@ class WalleCli < Formula
   desc     "Android Signature V2 Scheme签名下的新一代渠道包打包神器"
   homepage "https://github.com/Meituan-Dianping/walle"
   url      "https://github.com/leleliu008/walle-cli-wrapper/releases/download/v1.1.6/walle-cli-1.1.6.tar.gz"
-  sha256   "511d8977e294ef5795e079e8fa0137a11592e6d2f2148523267cd4fcd6233184"
+  sha256   "f1bd54c05edd1cfbbbde97ed2f78966409e911927bd377d61eaef8851634f285"
 
   depends_on "java"
 
