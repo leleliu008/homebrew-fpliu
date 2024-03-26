cf9b7494edf3e2df5f3eb3fed763e0abd3ab1ab7
leleliu008 <leleliu008@gmail.com>
2024-03-27 00:48:40 +0800

    add uppm package
    Signed-off-by: leleliu008 <leleliu008@gmail.com>

---
+++ Formula/uppm.rb
@@ -0,0 +1,28 @@
+class Uppm < Formula
+  desc     "A portable package manager for Unix-like system"
+  homepage "https://github.com/leleliu008/uppm"
+  head     "https://github.com/leleliu008/uppm.git", branch: "master"
+  url      "https://github.com/leleliu008/uppm.git", revision: "c53402c8f03c01b59052a17a65f0c297f15f2822"
+  version  "0.15.1"
+  license  "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "curl"
+  depends_on "jansson"
+  depends_on "libyaml"
+  depends_on "libgit2"
+  depends_on "libarchive"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build",   "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "#{bin}/uppm", "--help"
+  end
+end
